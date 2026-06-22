5813217d93411a400c60963cd8db8455d7c0c589
Bob Glickstein <bobg@emphatic.com>
2026-06-22 08:36:12 -0700

    Allow .mkv files.

--- handle.go
+++ handle.go
@@ -229,7 +229,7 @@ func (s *server) handleDir(w http.ResponseWriter, req *http.Request, subdir stri
 	s.objNames.Each(func(objName string) {
 		ext := filepath.Ext(objName)
 		switch ext {
-		case ".iso", ".m2ts", ".m4v", ".mp4", ".webm":
+		case ".iso", ".m2ts", ".m4v", ".mp4", ".webm", ".mkv":
 			// ok
 		default:
 			return
